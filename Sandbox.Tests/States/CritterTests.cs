4397038e8471622b8a197caec3767374e4b664cd
donatedsalt <mmohsinkhan3685@gmail.com>
2026-05-06 11:24:08 +0500

    test: add CritterTests for State pattern transition behavior
    - Replace placeholder UnitTest1 with CritterTests
    - Test critter transitions to HungryState when energy drops below threshold

---
+++ Sandbox.Tests/States/CritterTests.cs
@@ -0,0 +1,27 @@
+﻿using Sandbox.Entities;
+using Sandbox.States;
+using Sandbox.Strategies;
+
+namespace Sandbox.Tests.States;
+
+public class CritterTests
+{
+    [Fact]
+    public void Critter_ShouldTransitionToHungryState_WhenEnergyIsBelowThreshold()
+    {
+        var critter = new Critter(
+            name: "wolf",
+            health: 10,
+            energy: 10,
+            attack: 2,
+            defence: 1,
+            speed: 1,
+            vision: 1,
+            movementStrategy: new WalkStrategy()
+        );
+
+        critter.Energy = 5;
+        critter.Update();
+        Assert.IsType<HungryState>(critter.CurrentState);
+    }
+}
